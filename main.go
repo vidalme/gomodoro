3fc99436a83a253638e166fc97928d0e9fe9df92
andre <andrinho@gmail.com>
2024-09-16 08:10:42 -0300

    ainda organizando o codigo no main

--- main.go
+++ main.go
@@ -1,99 +1,118 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
-// import logica "github.com/vidalme/gomodoro/controller"
+const TEMPO_TRABALHO = 1
+const TEMPO_REPOUSO = 1
 
-// func timeLeft(t time.Time) int {
+type intervalo struct {
+	t         time.Time
+	d         time.Duration
+	mensagens [2]string
+}
 
-// }
+func menuMensagem() {
+	fmt.Println("+++++++  GOmodoro menu  +++++++")
+	fmt.Println("1 - Inicio rapido ( 45 min trabalho / 15 min )")
+	fmt.Println("2 - Configurar timer")
+	fmt.Println("3 - Sair")
+	fmt.Println("Digite uma opção: ")
+}
 
-// func justPlay() {
+func menuInicial() {
 
-// 	wt := convertMinutesToTime(duracao_inicial)
+	menuMensagem()
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
 
-// 	duracao_restante := timeLeft(wt)
-
-// 	fmt.Println(wt.Format(time.TimeOnly))
-
-// 	ticker := time.NewTicker(time.Second)
-// 	defer ticker.Stop()
-
-// 	done := make(chan bool)
-
-// 	go func() {
-// 		time.Sleep(time.Second * time.Duration(duracao_inicial))
-// 		done <- true
-// 	}()
-// 	for {
-// 		select {
-// 		case <-done:
-// 			fmt.Println("Done!")
-// 			return
-// 		case t := <-ticker.C:
-// 			tf := fmt.Sprintf("\r Current time: %v", t.Format(time.TimeOnly))
-// 			fmt.Print(tf)
-// 		}
-// 	}
-
-// }
+	switch input {
+	case "1":
+		fmt.Println("inicio rapido")
+		inicioRapido()
+	case "2":
+		fmt.Println("Configurar timer")
+	case "3":
+		fmt.Println("Sair do jogo")
+	default:
+		fmt.Println("opção invalida")
+	}
+}
 
 func convertMinutesToTime(mp int) time.Time {
 	h := mp / 60
 	m := mp % 60
-	s := mp * 60
-	// fmt.Printf("converteu os %d minutos em %d:%d:%d \n", mp, h, s, m)
 
-	// time.Date(0, 0, 0, h, m, s, 0, time.Local)
-
-	timeString := fmt.Sprintf("%02d:%02d:%02d", h, m, s)
-
-	// t, _ := time.Parse(time.TimeOnly, strconv.Itoa(h)+":"+strconv.Itoa(m)+":"+strconv.Itoa(s))
-	t, _ := time.Parse(time.TimeOnly, "05:05:01")
-	fmt.Println("------", timeString)
-	fmt.Println("------", t)
+	ts := fmt.Sprintf("%02d:%02d:00", h, m)
+	t, _ := time.Parse(time.TimeOnly, ts)
 
 	return t
 }
 
-func main() {
-	// logica.Iniciar()
-	// justPlay()
-	total_minutos := 65
-	total_segundos := total_minutos * 60
+func convertMinutesToDuration(mp int) time.Duration {
+	s := fmt.Sprintf("%dm", mp)
+	mpp, _ := time.ParseDuration(s)
+	return mpp
+}
 
-	countdownDuration := time.Duration(total_segundos) * time.Second
-	fmt.Println(countdownDuration)
+func inicio(tt int, tr int) {
 
-	countdown := convertMinutesToTime(total_minutos)
+	total_segundos := tt * 60
+	d := time.Duration(total_segundos) * time.Second
+	t := convertMinutesToTime(tt)
+	// d := convertMinutesToDuration(tt)
 
+	msgs := [2]string{
+		"Foco total ->",
+		"Parabens, você conclui um periodo de foco, descanse um pouco agora!",
+	}
+
+	i := intervalo{
+		d:         d,
+		t:         t,
+		mensagens: msgs,
+	}
+
+	rodaIntervalo(i)
+}
+
+func inicioRapido() {
+	inicio(TEMPO_TRABALHO, TEMPO_REPOUSO)
+}
+
+func inicioConfigurado() {}
+
+func rodaIntervalo(i intervalo) {
 	ticker := time.NewTicker(time.Second * 1)
 	defer ticker.Stop()
 
 	done := make(chan bool)
 
 	go func() {
-		time.Sleep(countdownDuration)
+		time.Sleep(i.d)
 		done <- true
 	}()
 
 	for {
 		select {
 		case <-done:
-			fmt.Println("Done!")
+			fmt.Printf("\n%v", i.mensagens[1])
 			return
 		case <-ticker.C:
-			// tf1 := fmt.Sprintf("\rCurrent time: %v", t.Format(time.TimeOnly))
-			// fmt.Print(tf1)
-
-			// tf2 := fmt.Sprintf("Countdown: %v \n", time.ParseDuration( countdownDuration.String() ))
-			// fmt.Print(tf2)
-
-			fmt.Println(countdownDuration)
-			fmt.Println(countdown.Format(time.TimeOnly))
+			i.t = i.t.Add(time.Second * -1)
+			tf := fmt.Sprintf("\r%v %v", i.mensagens[0], i.t.Format(time.TimeOnly))
+			fmt.Print(tf)
 		}
 	}
+}
+
+func main() {
+	// menuInicial()
+	inicioRapido()
 }
